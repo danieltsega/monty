c0090927e36fc45785162b3b9452d3ec32a21eba
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-23 07:46:51 -0700

    [Feature]: Adding a function that prints and pushes to a stack

---
+++ stack_fun1.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+/**
+ * _push - push int to a stack
+ * @stack: linked lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *top;
+	(void)line_number;
+
+	top = malloc(sizeof(stack_t));
+	if (top == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	top->n = var_global.push_arg;
+	top->next = *stack;
+	top->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = top;
+	*stack = top;
+}
+
+/**
+ * _pall - print all function
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	while (runner != NULL)
+	{
+		printf("%d\n", runner->n);
+		runner = runner->next;
+	}
+}
