3224ec51f96018c228723cd8033fe985c22b945a
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-23 07:40:47 -0700

    [Feature]: Adding another function that get the instruction and also that checks for number input

--- monty_fun.c
+++ monty_fun.c
@@ -44,3 +44,66 @@ void read_file(char *filename, stack_t **stack)
 	if (check == -1)
 		exit(-1);
 }
+
+/**
+ * get_op_func -  checks opcode and returns the correct function
+ * @str: the opcode
+ * Return: returns a functions, or NULL on failure
+ */
+instruct_func get_op_func(char *str)
+{
+	int i;
+
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
+	{
+		i++;
+	}
+
+	return (instruct[i].f);
+}
+
+/**
+ * isnumber - checks if a string is a number
+ * @str: string being passed
+ * Return: returns 1 if string is a number, 0 otherwise
+ */
+int isnumber(char *str)
+{
+	unsigned int i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[0] == '-')
+		{
+			i++;
+			continue;
+		}
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
