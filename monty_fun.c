acff63fcb0b24ca26c16d7bc02da059877c158ea
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-23 07:37:26 -0700

    [Feature]: Adding the file instruction reading function

---
+++ monty_fun.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+/**
+ * read_file - reads a bytecode file and runs commands
+ * @filename: pathname to file
+ * @stack: pointer to the top of the stack
+ */
+void read_file(char *filename, stack_t **stack)
+{
+	char *line;
+	size_t i = 0;
+	int line_count = 1;
+	instruct_func s;
+	int check;
+	int read;
+
+
+	var_global.file = fopen(filename, "r");
+
+	if (var_global.file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&var_global.buffer, &i, var_global.file)) != -1)
+	{
+		line = parse_line(var_global.buffer, stack, line_count);
+		if (line == NULL || line[0] == '#')
+		{
+			line_count++;
+			continue;
+		}
+		s = get_op_func(line);
+		if (s == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
+			exit(EXIT_FAILURE);
+		}
+		s(stack, line_count);
+		line_count++;
+	}
+	free(var_global.buffer);
+	check = fclose(var_global.file);
+	if (check == -1)
+		exit(-1);
+}
