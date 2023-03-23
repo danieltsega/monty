4e044efb62236823f9049f22ed875571f3756ecc
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-23 07:43:55 -0700

    [Feature]: Adding a parser and another function

--- monty_fun.c
+++ monty_fun.c
@@ -107,3 +107,35 @@ int isnumber(char *str)
 	}
 	return (1);
 }
+/**
+ * parse_line - parses a line for an opcode and arguments
+ * @line: the line to be parsed
+ * @stack: pointer to the head of the stack
+ * @line_number: the current line number
+ * Return: returns the opcode or null on failure
+ */
+char *parse_line(char *line, stack_t **stack, unsigned int line_number)
+{
+	char *op_code, *push, *arg;
+	(void)stack;
+
+	push = "push";
+	op_code = strtok(line, "\n ");
+	if (op_code == NULL)
+		return (NULL);
+
+	if (strcmp(op_code, push) == 0)
+	{
+		arg = strtok(NULL, "\n ");
+		if (isnumber(arg) == 1 && arg != NULL)
+		{
+			var_global.push_arg = atoi(arg);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (op_code);
+}
